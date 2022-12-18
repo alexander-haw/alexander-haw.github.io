5a0a06a16aee20c478065feda6ec2ee769e087c1
alexander-haw <clockworktux@gmail.com>
2022-12-19 02:12:03 +1300

    -

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@ author           = 'Alexander Haw'
 
 root_doc         = 'README'
 html_title       = "Alexander Haw"
-html_logo        = "_images/me_full_small.png"
+html_logo        = "images/me_full_small.png"
 html_theme       = 'bizstyle'
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
